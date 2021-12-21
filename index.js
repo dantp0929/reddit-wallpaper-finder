28e864543ca9bd0dd010333cd3ab7bf0bc48aaee
Daniel Phan <phand972@gmail.com>
2021-12-21 17:47:53 -0600

    Added filters

--- index.js
+++ index.js
@@ -1,28 +1,114 @@
+import { setCookie, getCookie, shuffle } from "./helpers.js";
 
-function getRandomImageFromChildren(children) {
-    var index = Math.floor(Math.random() * children.length);
-
-    return children[index].data.url;
+var wallpaperProperties = {
+  subreddit: "animewallpaper",
+  minWidth: 0,
+  minHeight: 0,
+  includeNSFW: true,
+  includeLandscape: true,
+  includePortrait: true,
+  minUpvotes: 0
 }
 
+// Cookies to prevent getting the same wallpapers in a row.
+var history = getCookie("history");
+if (history != "") {
+  history = JSON.parse(history);
+}
+else {
+  history = []
+}
 
-fetch('https://www.reddit.com/r/animewallpaper/top.json')
-  .then(function(res) {
-    return res.json();   // Convert the data into JSON
-  })
-  .then(function(res) {
-    console.log(res);   // Logs the data to the console
+/**
+ * Gets an image from threads in the subreddit.
+ * @param {array} threads The threads in the subreddit.
+ * @returns {string} url to an image.
+ */
+function getImageFromThreads(wallpaperProperties, threads) {
 
-    var fetchedImage = getRandomImageFromChildren(res.data.children);
+  for (var i = 0; i < threads.length; ++i) {
+    var thread = threads[i];
+    console.log(thread);
 
-    var background = document.getElementById("background").style.backgroundImage = `url(\"${fetchedImage}\")`;
-    
-    var wallpaper = document.getElementById("wallpaper");
-    var img = document.createElement("img");
-    img.src = fetchedImage
-    img.setAttribute("class", "wallpaper");
-    wallpaper.appendChild(img);
-  })
-  .catch(function(err) {
-    console.log(err);   // Log error if any
-  });
+    if (thread.data.preview != undefined) {
+      var landscape = thread.data.preview.images[0].source.height < thread.data.preview.images[0].source.width;
+      var portrait = thread.data.preview.images[0].source.height > thread.data.preview.images[0].source.width;
+
+      if (!history.includes(thread.data.name)
+        && thread.data.preview.images[0].source.height >= wallpaperProperties.minHeight 
+        && thread.data.preview.images[0].source.width >= wallpaperProperties.minWidth
+        && thread.data.ups >= wallpaperProperties.minUpvotes) {
+        
+        if (thread.data.over_18 && !wallpaperProperties.includeNSFW) {
+          continue;
+        }
+
+        if (landscape && !wallpaperProperties.includeLandscape) {
+          continue;
+        }
+
+        if (portrait && !wallpaperProperties.includePortrait) {
+          continue;
+        }
+
+        history.push(thread.data.name);
+        setCookie("history", JSON.stringify(history));
+        console.log(history);
+        return thread.data.url;
+      }
+    }
+  }
+  return null;
+}
+
+/**
+ * Get a new wallpaper from reddit.
+ * @param {Function} callback Function that takes a json as a parameter.
+ * @param {*} after Used in reddit's API to determine what threads to show.
+ */
+function getNewWallpapers(callback, after=null) {
+  var request = "";
+  if (after === null) {
+    request = `https://www.reddit.com/r/${wallpaperProperties.subreddit}/hot.json`;
+  }
+  else {
+    request = `https://www.reddit.com/r/${wallpaperProperties.subreddit}/hot.json?count=25&after=${after}`
+  }
+
+  console.log(request);
+
+  fetch(request)
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(json) {
+      callback(json);
+    })
+    .catch(function(err) {
+      console.log(err);   // Log error if any
+    });
+}
+
+/**
+ * Gets a new wallpaper and displays it.
+ * @param {String} subreddit The string after "r/" for reddit subreddits.
+ * @param {number} minWidth The minimum width an image can be.
+ * @param {number} minHeight The minimum height an image can be.
+ */
+function setRandomWallpaper(response) {
+  console.log(response);
+
+  var threads = response.data.children;
+  shuffle(threads);
+
+  var fetchedImage = getImageFromThreads(wallpaperProperties, threads);
+  if (fetchedImage === null) {
+    getNewWallpapers(setRandomWallpaper, threads[threads.length - 1].data.name);
+  }
+  else {
+    document.getElementById("background").style.backgroundImage = `url(\"${fetchedImage}\")`;
+    document.getElementById("wallpaper").src = fetchedImage;
+  }
+}
+
+getNewWallpapers(setRandomWallpaper);
